e58935d0aa326e22def709e9c4ddc755fc3219d1
ehill451 <124432327+ehill451@users.noreply.github.com>
2023-02-07 20:25:34 +1100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,28 @@
+print('Enter function.')
+f = input()
+if f == '*':
+    print('Enter variable 1')
+    a = (input())
+    print('Enter variable 2')
+    b = int(input())
+    print(a * b)
+elif f == '+':
+    print('Enter variable 1')
+    a = int(input())
+    print('Enter variable 2')
+    b = int(input())
+    print(a + b)
+elif f == '/':
+    print('Enter variable 1')
+    a = int(input())
+    print('Enter variable 2')
+    b = int(input())
+    print(a/b)
+elif f == '-':
+    print('Enter variable 1')
+    a = int(input())
+    print('Enter variable 2')
+    b = int(input())
+    print(a+b)
+else:
+    print( f'Is not a function')
